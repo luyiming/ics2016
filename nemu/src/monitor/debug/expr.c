2dea276faa7b2e2e3cdf0d8a90c9af5cb32232e3
tracer-ics2016 <tracer@njuics.org>
2016-09-08 00:19:18 +0000

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:01:55 UTC 2016 x86_64 GNU/Linux  00:19:18 up 14:16,  1 user,  load average: 0.12, 0.13, 0.54 e9bbed7d81769f09c8d7f6ec2c0c44221534141b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,11 +6,10 @@
 #include <sys/types.h>
 #include <regex.h>
 
+
+
 enum {
 	NOTYPE = 256, NUM, REG, VAR, LOR, LAN, EQ, NE, LE, GE, SHL, SHR, NEG, DEREF, NOP
-
-	/* TODO: Add more token types */
-
 };
 
 static bool is_operator(int type) {
@@ -137,9 +136,8 @@ static bool make_token(char *e) {
                          break;
                      }
                      case NOTYPE: break;
- 					default: tokens[nr_token++].type = rules[i].token_type; break;
+					 default: tokens[nr_token++].type = rules[i].token_type; break;
  				}
-
 				break;
 			}
 		}
@@ -149,7 +147,6 @@ static bool make_token(char *e) {
 			return false;
 		}
 	}
-
 	for(i = 0; i < nr_token; i ++) {
 		if(tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == '(' || is_operator(tokens[i - 1].type)) ) {
 			tokens[i].type = DEREF;
@@ -165,7 +162,6 @@ static bool make_token(char *e) {
 			tokens[i].type = NOP;
 		}
 	}
-
 	return true;
 }
 
@@ -409,6 +405,16 @@ static int calc() {
             // printf("get num: %d\n", value);
             v_st[++v_top] = value;
         }
+/*		else if(tokens[i].type == VAR) {
+			int value = get_symbol_value(tokens[i].str);
+			if(value == -1) {
+				success = false;
+				printf("cannot find symbol: %s\n", tokens[i].str);
+				return 0;
+			}
+			else
+				v_st[++v_top] = value;
+		} */
         else if(tokens[i].type == REG) {
             int value = 0;
             int j;
@@ -481,7 +487,150 @@ static int calc() {
     return v_st[0];
 }
 
+/*
+static int check_parentheses(int p, int q) {
+    // return value:
+    // 0  match
+    // -1 bad expression
+    // 1  good expression, not match
+    int st[64], top = 0;
+    UNUSED(st);
+    int i;
+    for(i = p; i <= q; i ++) {
+        if(tokens[i].type == '(') {
+            st[top++] = '(';
+        }
+        else if(tokens[i].type == ')') {
+            if(top == 0)
+                return -1;  // lack left parenthesis
+            top--;
+        }
+    }
+    if(top != 0)
+        return -1;  //lack right parenthesis
+    else if(tokens[p].type == '(' && tokens[q].type == ')')
+        return 0;
+    else
+        return 1;
+}
+
+
+static bool op_less_equal(int pt, int qt) {
+    if(qt == NOP)
+        return true;
+    if(pt == NEG || pt == DEREF)
+        return false;
+    else if (pt == '+' || pt == '-')
+        return true;
+    else {
+        if(qt == NEG || qt == DEREF)
+            return false;
+        else
+            return true;
+    }
+}
+
+
+static int eval(int p, int q) {
+    if(p > q) {
+//                Bad expression
+        return 0;
+    }
+    else if(p == q) {
+    // Single token.
+    //       * For now this token should be a number.
+    //               * Return the value of the number.
+        if(tokens[p].type == NUM) {
+            int value = 0;
+            if(strncmp(tokens[p].str, "0x", 2) == 0) { // Hex
+                int i, len = strlen(tokens[p].str);
+                for(i = 2; i < len; i ++) {
+                    int tmp = tokens[p].str[i] - '0';
+                    if(tmp > 9 || tmp < 0)
+                        tmp = tokens[p].str[i] - 'a' + 10;
+                    value = value * 16 + tmp;
+                }
+            }
+            else { // Dec
+                int i, len = strlen(tokens[p].str);
+                for(i = 0; i < len; i ++) {
+                    value = value * 10 + tokens[p].str[i] - '0';
+                }
+            }
+            return value;
+        }
+        else if(tokens[p].type == REG) {
+            uint32_t value = 0;
+            int i;
+            for(i = R_EAX; i <= R_EDI; i ++) {
+                if(strcmp(tokens[p].str, regsl[i]) == 0) {
+                    value = reg_l(i);
+                    return value;
+                }
+            }
+            for(i = R_AX; i <= R_DI; i ++) {
+                if(strcmp(tokens[p].str, regsw[i]) == 0) {
+                    value = reg_w(i);
+                    return value;
+                }
+            }
+            for(i = R_AL; i <= R_BH; i ++) {
+                if(strcmp(tokens[p].str, regsb[i]) == 0) {
+                    value = reg_b(i);
+                    return value;
+                }
+            }
+            if(strcmp(tokens[p].str, "eip") == 0)
+                return cpu.eip;
+            printf("undefined register\n");
+            return -1;
+        }
+        else
+            return 0;
+    }
+    else if(check_parentheses(p, q) == 0) {
+    // The expression is surrounded by a matched pair of parentheses.
+    //       * If that is the case, just throw away the parentheses.
+        return eval(p + 1, q - 1);
+    }
+    else {
+        int i, in_bracket = 0;
+        int op = 0, op_type = NOP; // initial op_type should be large enough
+        // op = the position of dominant operator in the token expression;
+        for(i = p; i <= q; i++) {
+            if(tokens[i].type == '(')
+                in_bracket++;
+            else if(tokens[i].type == ')')
+                in_bracket--;
+            else if(is_operator(tokens[i].type) && !in_bracket) {
+                if(op_less_equal(tokens[i].type, op_type)) {
+                    op = i;
+                    op_type = tokens[i].type;
+                }
+            }
+        }
+
+        switch(op_type) {
+            case '+': return eval(p, op - 1) + eval(op + 1, q);
+            case '-': return eval(p, op - 1) - eval(op + 1, q);
+            case '*': return eval(p, op - 1) * eval(op + 1, q);
+            case '/': return eval(p, op - 1) / eval(op + 1, q);
+            case NEG: return -eval(op + 1, q);
+            case DEREF:
+            {
+                int t_addr = eval(op + 1, q);
+                Assert(t_addr >= 0, "address out of range: 0x%x\t %d\n", t_addr, t_addr);
+                uint32_t value = swaddr_read(t_addr, 4);
+                return value;
+            }
+            default: Assert(0, "bad expression\n");
+        }
+    }
+}
+*/
+
 int expr(char *e, bool *suc) {
+
     success = true;
     v_top = -1, op_top = -1;
 
