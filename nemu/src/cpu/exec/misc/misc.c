5eccf063d44ec9132ed8394e290a722007c8bcdf
tracer-ics2016 <tracer@njuics.org>
2016-10-05 21:49:27 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  21:49:27 up  4:33,  1 user,  load average: 0.37, 0.42, 0.43 6ca851def18bbb39d4f7cecff64c7a6922fd8cec

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -23,3 +23,12 @@ make_helper(lea) {
 	print_asm("leal %s,%%%s", op_src->str, regsl[m.reg]);
 	return 1 + len;
 }
+
+make_helper(leave) {
+    cpu.esp = cpu.ebp;
+    cpu.ebp = swaddr_read(cpu.esp, 4);
+    cpu.esp += 4;
+
+    print_asm("leave");
+    return 1;
+}
