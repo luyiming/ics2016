0a1266a64a7a0cad2743179137dd514331ee77aa
tracer-ics2016 <tracer@njuics.org>
2016-10-05 21:36:28 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  21:36:28 up  4:20,  1 user,  load average: 0.87, 0.41, 0.43 f007cdd7ea8ecb09db15518011d286c31806d4e6

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -2,14 +2,25 @@
 
 #define instr push
 
+#if DATA_BYTE == 1
+// push imm8 stores 4 bytes
 static void do_execute () {
-	cpu.esp -= DATA_BYTE;
-	swaddr_write(cpu.esp, DATA_BYTE, op_src->val);
-	print_asm_template1();
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, op_src->val);
+
+    print_asm_template1();
 }
+
+#endif
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 
+static void do_execute () {
+    cpu.esp -= DATA_BYTE;
+    swaddr_write(cpu.esp, DATA_BYTE, op_src->val);
+
+    print_asm_template1();
+}
 /*
 make_helper(concat(push_m_, SUFFIX)) {
 	int len = concat(decode_rm_, SUFFIX)(eip + 1);
@@ -18,9 +29,8 @@ make_helper(concat(push_m_, SUFFIX)) {
 }
 */
 
-make_instr_helper(r)
 make_instr_helper(rm)
-
+make_instr_helper(r)
 #endif
 
 make_instr_helper(i)
