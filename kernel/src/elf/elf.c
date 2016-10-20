2be80a53d4bef70bb89a34c5c5e84ae70c1bd057
tracer-ics2016 <tracer@njuics.org>
2016-10-20 09:15:11 +0800

    >  compile kernel 151220066 luyiming Linux 79fb2e46c627 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  09:15:11 up  1:20,  1 user,  load average: 0.33, 0.20, 0.15 21f981a8f99589c94bccf7d80c52d3b2dd7c5ed

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,25 +31,24 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	ph = (Elf32_Phdr *)(buf + elf->e_phoff);
+	uint16_t i = 0;
+	for(i = 0; i < elf->e_phnum; ++i) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-			/* TODO: read the content of the segment from the ELF file 
+			/* read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
-			 
-			/* TODO: zero the memory region 
-			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
+			 ramdisk_read((uint8_t *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 
+			/* zero the memory region [VirtAddr + FileSiz, VirtAddr + MemSiz) */
+			 memset((uint8_t *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
