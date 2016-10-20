d8213a2935497daf0daae4135852f5953c6bca9a
tracer-ics2016 <tracer@njuics.org>
2016-10-20 19:32:31 +0800

    >  run 151220066 luyiming Linux 79fb2e46c627 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  19:32:31 up  3:20,  1 user,  load average: 0.55, 0.48, 0.41 54dfb620cab838dca451b98fe9563344dde3868

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -9,12 +9,10 @@ char buf[128];
 int main() {
 	init_FLOAT_vfprintf();
 
-#ifdef LINUX_RT/*
+#ifdef LINUX_RT
 	printf("%f\n", FLOAT_ARG(0x00010000));
 	printf("%f\n", FLOAT_ARG(0x00013333));
-	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);*/
-	printf("%f\n", FLOAT_ARG(0x00010000));
-	printf("%lld\n", FLOAT_ARG(0x00010000));
+	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
 #else
 	sprintf(buf, "%f", FLOAT_ARG(0x00010000));
 	nemu_assert(strcmp(buf, "1.000000") == 0);
