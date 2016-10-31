9589a68b4bd13f138abab5a95cc806bbe2d250c5
tracer-ics2016 <tracer@njuics.org>
2016-10-31 09:46:29 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  09:46:29 up  2:02,  1 user,  load average: 0.28, 0.14, 0.10 f669e33cfcfb4c09a42aa3ffc787730898d8c77a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,6 +3,35 @@
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
+#define CACHE_WIDTH 16
+#define BLOCK_WIDTH 6
+#define SET_BLOCK_WIDTH 3
+#define SET_WIDTH (CACHE_WIDTH - BLOCK_WIDTH - SET_BLOCK_WIDTH)
+#define TAG_WIDTH (27 - BLOCK_WIDTH - SET_WIDTH)
+
+#define BLOCK_SIZE (1 << BLOCK_WIDTH)
+#define NR_SET (1 << SET_WIDTH)
+#define NR_BLOCK (1 << SET_BLOCK_WIDTH)
+
+typedef union {
+	struct {
+		uint32_t block_addr	: BLOCK_WIDTH;
+		uint32_t nr_set	: SET_WIDTH;
+		uint32_t tag	: TAG_WIDTH;
+	};
+	uint32_t addr;
+} cache_addr;
+
+typedef struct {
+    uint8_t data[BLOCK_SIZE];
+    bool valid;
+    uint32_t tag;
+} Block;
+
+typedef struct {
+    Block blocks[NR_SET][NR_BLOCK];
+} Cache;
+
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
