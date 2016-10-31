a02a9764b63d8c991a8c967700a4a6d47ae70a15
tracer-ics2016 <tracer@njuics.org>
2016-10-31 21:01:25 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  21:01:25 up  2:40,  1 user,  load average: 0.23, 0.13, 0.13 b477cb663375d05e5b7bc220525473f06381256d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -94,7 +94,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
     }
     printf("\ndram:\n");
     for(k = 0; k < BLOCK_SIZE; k++) {
-        printf("%x ", dram_read(addr + k, 1) && 0xff);
+        printf("%x ", dram_read(addr + k, 1) & 0xff);
     }
     memcpy(&data, &cache[nr_set][i].data[block_addr], len);
 	Assert((dram_read(addr, len) & (~0u >> ((4 - len) << 3))) == data, "cache_read fail");
