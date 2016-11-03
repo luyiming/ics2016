61943ba19abe803612696a46ef026cb89b491381
tracer-ics2016 <tracer@njuics.org>
2016-11-03 10:04:49 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  10:04:49 up  1:08,  1 user,  load average: 0.49, 0.46, 0.28 fa4bae4e889d4145bd1f1bb291a35791e9bc654

---
+++ nemu/src/memory/cache.c
@@ -0,0 +1,128 @@
+#include "common.h"
+#include "stdlib.h"
+
+uint32_t dram_read(hwaddr_t, size_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
+
+#define CACHE_WIDTH 16
+#define CACHE_SIZE (64 << 10)
+
+#define COL_WIDTH 6
+#define ROW_WIDTH 3
+#define SET_WIDTH 7  // == (CACHE_WIDTH - COL_WIDTH - ROW_WIDTH)
+#define TAG_WIDTH (27 - COL_WIDTH - SET_WIDTH)
+
+#define NR_COL (1 << COL_WIDTH)
+#define NR_ROW (1 << ROW_WIDTH)
+#define NR_SET (1 << SET_WIDTH)
+
+#define HW_MEM_SIZE (1 << (COL_WIDTH + SET_WIDTH + TAG_WIDTH))
+#define COL_MASK ((1 << COL_WIDTH) - 1)
+
+typedef union {
+	struct {
+		uint32_t col: COL_WIDTH;
+		uint32_t set: SET_WIDTH;
+		uint32_t tag: TAG_WIDTH;
+	};
+	uint32_t addr;
+} cache_addr;
+
+typedef struct {
+    uint8_t data[NR_COL];
+    bool valid;
+    uint32_t tag;
+} Block;
+
+Block cache[NR_SET][NR_ROW];
+
+void init_cache() {
+	int i, j;
+	for(i = 0; i < NR_SET; i ++) {
+		for(j = 0; j < NR_ROW; j ++) {
+			cache[i][j].valid = false;
+		}
+	}
+}
+
+int cache_miss(hwaddr_t addr) {
+    int i, j;
+    cache_addr caddr;
+    caddr.addr = addr;
+    uint32_t set = caddr.set;
+    for(i = 0; i < NR_ROW; i++) {
+        if(cache[set][i].valid == false)
+            break;
+    }
+    if(i == NR_ROW) {
+        i = rand() % NR_ROW;
+    }
+    addr = addr & ~COL_MASK;  // set col filed to zero
+    for(j = 0; j < NR_COL; j += 4) {
+        unalign_rw(cache[set][i].data + j, 4) = dram_read(addr + j, 4);
+    }
+    Assert(j == NR_COL, "j != NR_COL");
+    cache[set][i].valid = true;
+    cache[set][i].tag = caddr.tag;
+    return i;
+}
+
+uint32_t cache_read(hwaddr_t addr, size_t len) {
+    Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
+
+    cache_addr caddr;
+    caddr.addr = addr;
+    uint32_t col = caddr.col;
+    uint32_t set = caddr.set;
+    uint32_t tag = caddr.tag;
+    uint32_t offset = 0;
+    uint8_t temp[8];
+    int i;
+    if(col + len > NR_COL) {
+        unalign_rw(temp + NR_COL - col, 4) = cache_read(addr + NR_COL - col, len);
+        Assert(addr + NR_COL - col == ((addr + NR_COL - col) & ~COL_MASK),
+               "second cache read fail, addr: %x", addr + NR_COL - col);
+        len = len - (NR_COL - col);
+    }
+    for(i = 0; i < NR_ROW; i++) {
+        if(cache[set][i].valid && cache[set][i].tag == tag) {
+            memcpy(temp, &cache[set][i].data[col], len);
+            return unalign_rw(temp + offset, 4);
+        }
+    }
+    // cache miss handle:
+    i = cache_miss(addr);
+    memcpy(temp, &cache[set][i].data[col], len);
+    return unalign_rw(temp + offset, 4);
+}
+
+void cache_write(hwaddr_t addr, size_t len, uint32_t data) {
+    Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
+
+    int i;
+    cache_addr caddr;
+    caddr.addr = addr;
+    uint32_t col = caddr.col;
+    uint32_t set = caddr.set;
+    uint32_t tag = caddr.tag;
+    for(i = 0; i < NR_ROW; i++) {
+        if(cache[set][i].valid && cache[set][i].tag == tag) {
+            memcpy(&cache[set][i].data[col], &data, len);
+            dram_write(addr, len, data);
+            return;
+        }
+    }
+    i = cache_miss(addr);
+    memcpy(&cache[set][i].data[col], &data, len);
+    dram_write(addr, len, data);
+}
+
+#undef CACHE_WIDTH
+#undef CACHE_SIZE
+#undef COL_WIDTH
+#undef ROW_WIDTH
+#undef SET_WIDTH
+#undef TAG_WIDTH
+#undef NR_COL
+#undef NR_ROW
+#undef NR_SET
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,131 +3,16 @@
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
-
-#define CACHE_WIDTH 16
-#define BLOCK_WIDTH 6
-#define SET_BLOCK_WIDTH 3
-#define SET_WIDTH (CACHE_WIDTH - BLOCK_WIDTH - SET_BLOCK_WIDTH)
-#define TAG_WIDTH (27 - BLOCK_WIDTH - SET_WIDTH)
-
-#define BLOCK_SIZE (1 << BLOCK_WIDTH)
-#define NR_SET (1 << SET_WIDTH)
-#define NR_SET_BLOCK (1 << SET_BLOCK_WIDTH)
-
-typedef union {
-	struct {
-		uint32_t block_addr	: BLOCK_WIDTH;
-		uint32_t nr_set	: SET_WIDTH;
-		uint32_t tag	: TAG_WIDTH;
-	};
-	uint32_t addr;
-} cache_addr;
-
-typedef struct {
-    uint8_t data[BLOCK_SIZE];
-    bool valid;
-    uint32_t tag;
-} Block;
-
-Block cache[NR_SET][NR_SET_BLOCK];
-
-
-void init_cache() {
-	int i, j;
-	for(i = 0; i < NR_SET; i ++) {
-		for(j = 0; j < NR_SET_BLOCK; j ++) {
-			cache[i][j].valid = false;
-		}
-	}
-}
-
-int cache_miss(hwaddr_t addr) {
-    int i, j;
-    int k;
-    uint32_t data;
-    cache_addr temp;
-    temp.addr = addr;
-    uint32_t nr_set = temp.nr_set;
-    for(i = 0; i < NR_SET_BLOCK; i++) {
-        if(cache[nr_set][i].valid == false)
-            break;
-    }
-    if(i == NR_SET_BLOCK) {
-        i = rand() % NR_SET_BLOCK;
-    }
-    addr = addr & ~((1 << BLOCK_WIDTH) - 1);
-    for(j = 0; j < BLOCK_SIZE; j += 4) {
-        data = dram_read(addr + j, 4);
-        memcpy(cache[nr_set][i].data + j, &data, 4);
-    }
-    Assert(j == BLOCK_SIZE, "j != BLOCK_SIZE");
-    for(k = 0; k < BLOCK_SIZE; k++) {
-        Assert(cache[nr_set][i].data[k] == (dram_read(addr + k, 1) & 0xff), "cache_miss fail");
-    }
-    cache[nr_set][i].valid = true;
-    cache[nr_set][i].tag = temp.tag;
-    return i;
-}
-
-void cache_read_helper(hwaddr_t addr, size_t len, uint8_t* data) {
-    int i;
-    cache_addr temp;
-    temp.addr = addr;
-    uint32_t block_addr = temp.block_addr;
-    uint32_t nr_set = temp.nr_set;
-    uint32_t tag = temp.tag;
-    for(i = 0; i < NR_SET_BLOCK; i++) {
-        if(cache[nr_set][i].valid && cache[nr_set][i].tag == tag) {
-            memcpy(data, &cache[nr_set][i].data[block_addr], len);
-            printf("cache_get addr:%x len:%d data:%x\n", addr, len, *data);
-            printf("dram addr:%x data:%x \n", addr, dram_read(addr, len) & (~0u >> ((4 - len) << 3)));
-            return;
-        }
-    }
-    i = cache_miss(addr);
-    memcpy(data, &cache[nr_set][i].data[block_addr], len);
-    printf("cache_miss addr:%x len:%d data:%x\n", addr, len, *data);
-    printf("dram addr:%x data:%x \n", addr, dram_read(addr, len) & (~0u >> ((4 - len) << 3)));
-    return;
-}
-
-uint32_t cache_read(hwaddr_t addr, size_t len) {
-    uint32_t offset = addr & ~((1 << BLOCK_WIDTH) - 1);
-    uint8_t temp[32];
-    uint32_t *res = (uint32_t*)temp;
-    cache_read_helper(addr, len, temp);
-    if(offset + len > BLOCK_SIZE) {
-        cache_read_helper(addr + BLOCK_SIZE - offset, len - BLOCK_SIZE + offset, temp);
-    } 
-    return *res;
-}
-
-void cache_write(hwaddr_t addr, size_t len, uint32_t data) {
-    int i;
-    cache_addr temp;
-    temp.addr = addr;
-    uint32_t block_addr = temp.block_addr;
-    uint32_t nr_set = temp.nr_set;
-    uint32_t tag = temp.tag;
-    for(i = 0; i < NR_SET_BLOCK; i++) {
-        if(cache[nr_set][i].valid && cache[nr_set][i].tag == tag) {
-            memcpy(&cache[nr_set][i].data[block_addr], &data, len);
-            dram_write(addr, len, data);
-            return;
-        }
-    }
-    i = cache_miss(addr);
-    memcpy(&cache[nr_set][i].data[block_addr], &data, len);
-    dram_write(addr, len, data);
-}
+uint32_t cache_read(hwaddr_t addr, size_t len);
+void cache_write(hwaddr_t addr, size_t len, uint32_t data);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-    uint32_t data1 = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-    uint32_t data2 = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+    //uint32_t data1 = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+    //uint32_t data2 = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-    Assert(data1 == data2, "hwaddr_read fail, addr:%x, dram:%x cache:%x", addr, data1, data2);
+    //Assert(data1 == data2, "hwaddr_read fail, addr:%x, dram:%x cache:%x", addr, data1, data2);
 	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
@@ -157,4 +42,3 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 #endif
 	lnaddr_write(addr, len, data);
 }
-
