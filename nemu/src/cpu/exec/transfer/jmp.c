0c9214cf992ba7390f8885b8b1469d9ded36b548
tracer-ics2016 <tracer@njuics.org>
2016-11-26 16:36:38 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  16:36:38 up  3:52,  1 user,  load average: 0.48, 0.45, 0.41 8da80e9dcb8a5488fb0b547a679ede2d92e1f52d

--- nemu/src/cpu/exec/transfer/jmp.c
+++ nemu/src/cpu/exec/transfer/jmp.c
@@ -1,5 +1,17 @@
 #include "cpu/exec/helper.h"
 
+void load_sreg(uint32_t);
+
+make_helper(ljmp) {
+	swaddr_t addr = instr_fetch(eip + 1, 4);
+	uint16_t sreg = instr_fetch(eip + 5, 2);
+	cpu.CS = sreg;
+	load_sreg(R_CS);
+	cpu.eip = addr - 7;
+	print_asm("ljmp $0x%x,$0x%x", sreg, addr);
+	return 7;
+}
+
 make_helper(jmp_i_b) {
     int8_t rel = instr_fetch(eip + 1, 1);
     cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
