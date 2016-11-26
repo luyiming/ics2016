68e56ada81ceb8e38066d515c98b5ac945fee562
tracer-ics2016 <tracer@njuics.org>
2016-11-26 16:49:41 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  16:49:41 up  4:05,  1 user,  load average: 0.59, 0.48, 0.46 d2eb579727616ff4fa1b54b6fcd6820536509aef

--- nemu/src/memory/seg.c
+++ nemu/src/memory/seg.c
@@ -9,17 +9,17 @@ void init_seg() {
 }
 
 void load_sreg(uint32_t sreg) {
-	//load into cache
-    int i;
+	// load seg_descriptor into cache
     uint8_t tmp[8];
-    for(i = 0; i < 8; ++ i){
+	int i;
+    for(i = 0; i < 8; i++) {
         tmp[i] = lnaddr_read(cpu.GDTR.Base + cpu.SR[sreg].Index * 8 + i, 1);
     }
     SegDesc *segdesc = (SegDesc*)tmp;
     uint32_t limit = (segdesc->limit_19_16 << 16) + segdesc->limit_15_0;
 	uint32_t base = (segdesc->base_31_24 << 24) + (segdesc->base_23_16 << 16) + segdesc->base_15_0;
-    //Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x", cpu.GDTR.Base + cpu.SR[sreg].Index * 8);
-	//Assert(cpu.SR[sreg].Index * 8 < limit, "Segment overflow!");
+    // Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x", cpu.GDTR.Base + cpu.SR[sreg].Index * 8);
+	Assert(cpu.SR[sreg].Index * 8 < limit, "Segment overflow");
 	cpu.SR_cache[sreg].valid = true;
 	cpu.SR_cache[sreg].limit = limit;
 	cpu.SR_cache[sreg].base = base;
@@ -32,9 +32,10 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg) {
     // 01 CS
     // 10 SS
     // 11 DS
-    assert(sreg == 0 || sreg == 1 || sreg == 2 || sreg == 3);
+    Assert(sreg >= 0 && sreg <= 3, "sreg index out of range");
     if(cpu.CR0.protect_enable == 0)
         return addr;
+
     if(cpu.SR_cache[sreg].valid == 0)
         load_sreg(sreg);
 	return cpu.SR_cache[sreg].base + addr;
