9689595bdd910d3f9669a141a933f849e5a2ce32
tracer-ics2016 <tracer@njuics.org>
2016-12-18 21:57:03 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:59:10 UTC 2016 x86_64 GNU/Linux  21:57:03 up 44 min,  1 user,  load average: 0.52, 0.36, 0.41 3a7877e92b6b91fda547d375d4399e080a58958

---
+++ nemu/src/cpu/exec/raise_instr.c
@@ -0,0 +1,33 @@
+#include "../../lib-common/x86-inc/mmu.h"
+#include "cpu/reg.h"
+#include <setjmp.h>
+extern jmp_buf jbuf;
+
+uint32_t lnaddr_read(lnaddr_t, size_t);
+void swaddr_write(swaddr_t, size_t, uint32_t, uint8_t);
+
+void load_sreg(uint32_t);
+
+void raise_intr(uint8_t NO) {
+  /*	TODO:	Trigger	an	interrupt/exception	with	``NO''.
+	 	*	That	is,	use	``NO''	to	index	the	IDT.
+	 	*/
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eflags, R_SS);
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.CS, R_SS);
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eip, R_SS);
+
+	uint8_t tmp[8];
+	int i;
+	for(i = 0; i < 8; ++ i)
+		tmp[i] = lnaddr_read(cpu.IDTR.Base + NO * 0x8 + i, 1);
+	GateDesc *gde = (GateDesc*)tmp;
+
+	cpu.CS = gde->segment;
+	load_sreg(R_CS);
+	cpu.eip = (gde->offset_31_16 << 16) + gde->offset_15_0;
+	/* Jump back to cpu_exec() */
+	longjmp(jbuf, 1);
+}
