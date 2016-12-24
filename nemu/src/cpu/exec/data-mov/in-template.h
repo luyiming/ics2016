6dd239aba8a23e5f2bde8dbecf97b2040a56891b
tracer-ics2016 <tracer@njuics.org>
2016-12-24 11:09:47 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:59:10 UTC 2016 x86_64 GNU/Linux  11:09:47 up  2:49,  1 user,  load average: 0.57, 0.69, 0.60 a3c2c1af3b1d3af22d7d98fe4af2c9c37c56e74d

--- nemu/src/cpu/exec/data-mov/in-template.h
+++ nemu/src/cpu/exec/data-mov/in-template.h
@@ -1,3 +1,4 @@
+/*
 #include "cpu/exec/template-start.h"
 
 #define instr in
@@ -17,5 +18,27 @@ make_helper(concat(in_imm_, SUFFIX)) {
 	return 2;
 }
 
+
+#include "cpu/exec/template-end.h"
+*/
+
+#include "cpu/exec/template-start.h"
+
+#define instr in
+
+uint32_t pio_read(ioaddr_t, size_t);
+
+make_helper(concat(in_, SUFFIX)) {
+//	if (cpu.cr0.paging==1) {
+		concat(reg_, SUFFIX)(R_EAX) = pio_read(reg_w(R_DX), DATA_BYTE);
+//	}
+	print_asm("in" " (%%dx),%%%s", concat(regs, SUFFIX)[R_EAX]);
+	return 1;
+}
+
+make_helper(concat(in_imm_, SUFFIX)) {
+	panic("not implemented");
+	return 1;
+}
 
 #include "cpu/exec/template-end.h"
