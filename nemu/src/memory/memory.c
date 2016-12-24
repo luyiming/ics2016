238e3f39e71762dfb4a93220bdd0d0b3903d05c7
tracer-ics2016 <tracer@njuics.org>
2016-12-24 11:40:26 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:59:10 UTC 2016 x86_64 GNU/Linux  11:40:26 up  3:19,  1 user,  load average: 0.57, 0.67, 0.75 b03af076caf7d92af15591db721979d78413b444

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -58,7 +58,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
 	if(cpu.CR0.paging == 0)
 		return hwaddr_write(addr, len, data);
-	if ((addr & 0xfff) + len > 0x1000) {
+	if (false/*(addr & 0xfff) + len > 0x1000*/) {
 		assert(0);
 	}
 	else {
