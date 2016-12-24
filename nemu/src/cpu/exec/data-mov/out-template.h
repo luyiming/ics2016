ce9306ce62c50bd565ecb6994d6745b4afa4ca47
tracer-ics2016 <tracer@njuics.org>
2016-12-24 10:31:48 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:59:10 UTC 2016 x86_64 GNU/Linux  10:31:48 up  2:11,  1 user,  load average: 0.60, 0.45, 0.38 29199075783974abe24ded1c2a44190559acb6c0

--- nemu/src/cpu/exec/data-mov/out-template.h
+++ nemu/src/cpu/exec/data-mov/out-template.h
@@ -1,4 +1,4 @@
-#include "cpu/exec/template-start.h"
+/*#include "cpu/exec/template-start.h"
 
 #define instr out
 
@@ -18,5 +18,25 @@ make_helper(concat(out_imm_, SUFFIX)) {
 	return 2;
 }
 
+
+#include "cpu/exec/template-end.h"
+*/
+
+#include "cpu/exec/template-start.h"
+
+#define instr out
+
+void pio_write(ioaddr_t, size_t, uint32_t);
+
+make_helper(concat(out_, SUFFIX)) {
+	pio_write(reg_w(R_DX), 1, reg_b(R_AL));
+	print_asm("out" " %%al,(%%dx)");
+	return 1;
+}
+
+make_helper(concat(out_imm_, SUFFIX)) {
+	panic("not implemented");
+	return 0;
+}
 
 #include "cpu/exec/template-end.h"
