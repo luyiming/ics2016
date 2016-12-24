6dc6a4a1162d07d8deb43446f380f6aa0e1526ee
tracer-ics2016 <tracer@njuics.org>
2016-12-24 09:59:32 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:59:10 UTC 2016 x86_64 GNU/Linux  09:59:32 up  1:38,  1 user,  load average: 0.60, 0.40, 0.30 4222f7dff956c49f791a2898025f3db056c776

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -9,15 +9,27 @@ void cache_write(hwaddr_t addr, size_t len, uint32_t data);
 lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg);
 hwaddr_t page_translate(lnaddr_t addr);
 
+uint32_t mmio_read(hwaddr_t, size_t, int);
+void mmio_write(hwaddr_t, size_t, uint32_t, int);
+uint32_t is_mmio(hwaddr_t);
+
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int num = is_mmio(addr);
+	if (num == -1)
+		return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	else
+		return mmio_read(addr, len, num) & (~0u >> ((4 - len) << 3));
 	// return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	cache_write(addr, len, data);
+	int num = is_mmio(addr);
+	if(num == -1)
+		cache_write(addr, len, data);
+	else
+		mmio_write(addr, len, data, num);
 	// dram_write(addr, len, data);
 }
 
