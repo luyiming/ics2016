81b25a327c9dd5d276b286432af95f307683463e
tracer-ics2016 <tracer@njuics.org>
2017-01-10 13:28:13 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-57-generic #78-Ubuntu SMP Fri Dec 9 23:50:32 UTC 2016 x86_64 GNU/Linux  13:28:13 up 22:34,  1 user,  load average: 0.16, 0.38, 0.53 325051292c414516ef82d8daed187664f0a5a314

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -1,5 +1,8 @@
 #include "nemu.h"
 
+void init_tlb();
+hwaddr_t tlb_read(lnaddr_t addr);
+
 typedef union {
 	struct {
 		uint32_t offset	:	12;
@@ -7,41 +10,36 @@ typedef union {
 		uint32_t dir	:	10;
 	};
 	uint32_t val;
-} line_addr;
+} addr_page_t;
 
 uint32_t hwaddr_read(hwaddr_t, size_t);
 
 PTE page_read(lnaddr_t addr){
-	line_addr lnaddr;
+	addr_page_t lnaddr;
 	lnaddr.val = addr;
 
-	// first level page table - page directory entry
+	// page directory entry
 	PDE dir_entry;
-	//Log("addr = 0x%x, dir = 0x%x, CR3.val = 0x%x, dir_entry_addr = 0x%x", addr, lnaddr.dir,cpu.CR3.val, (cpu.CR3.page_directory_base << 12) + 4 * lnaddr.dir);
 	dir_entry.val = hwaddr_read((cpu.CR3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
-	//Assert(dir_entry.present == 1, "dir_entry is not valid!  addr = 0x%x, dir = 0x%x", addr, lnaddr.dir);
 
-	// second level page table - page table entry
+	// page table entry
 	PTE page_table_entry;
 	page_table_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
-	//Assert(page_table_entry.present == 1, "page_table_entry is not valid!  addr = 0x%x, page_frame = 0x%x, page_table_entry = 0x%x",
-	//		addr, dir_entry.page_frame, page_table_entry.val);
+
 	return page_table_entry;
 }
 
-void init_tlb();
-hwaddr_t tlb_read(lnaddr_t addr);
-uint32_t this_CR3;
 hwaddr_t page_translate(lnaddr_t addr) {
-	if(this_CR3 != cpu.CR3.val) {
+    static uint32_t tmp_CR3;
+	if(tmp_CR3 != cpu.CR3.val) {
 		init_tlb();
-		this_CR3 = cpu.CR3.val;
+		tmp_CR3 = cpu.CR3.val;
 	}
 	return tlb_read(addr);
 }
 
 void debug_page(lnaddr_t addr){
-	line_addr lnaddr;
+	addr_page_t lnaddr;
 	lnaddr.val = addr;
 	printf("lnaddr = 0x%x, dir = 0x%x, page = 0x%x, offset = 0x%x\n", lnaddr.val, lnaddr.dir, lnaddr.page, lnaddr.offset);
 	printf("CR3.page_directory_base = 0x%x, dir_entry_addr = 0x%x\n", cpu.CR3.page_directory_base, (cpu.CR3.page_directory_base << 12) + 4 * lnaddr.dir);
