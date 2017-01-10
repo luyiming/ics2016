f56041f28ce925d4ec3b99d6aa35b84d9eaf1683
tracer-ics2016 <tracer@njuics.org>
2017-01-10 13:53:51 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-57-generic #78-Ubuntu SMP Fri Dec 9 23:50:32 UTC 2016 x86_64 GNU/Linux  13:53:51 up 23:00,  1 user,  load average: 0.95, 0.94, 0.89 33c1df366f17c51482e9c0dc1c706281af669910

--- nemu/src/memory/tlb.c
+++ nemu/src/memory/tlb.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "time.h"
 #include "../../lib-common/x86-inc/mmu.h"
+
 #define ADDRESS_BITS 32
 
 #define OFFSET_WIDTH 12
@@ -10,14 +11,8 @@
 
 #define NR_LINE (1 << LINE_WIDTH)
 
-
-typedef union {
-	struct {
-		uint32_t offset	: OFFSET_WIDTH;
-		uint32_t tag	: TAG_WIDTH;
-	};
-	uint32_t val;
-} tlb_addr;
+#define TLB_TAG(addr) 	 ((addr >> OFFSET_WIDTH) & ((1 << TAG_WIDTH) - 1))
+#define TLB_OFFSET(addr) (addr & ((1 << OFFSET_WIDTH) - 1))
 
 typedef	struct {
 	PTE pte;
@@ -35,20 +30,18 @@ void init_tlb(){
 
 PTE page_read(lnaddr_t);
 hwaddr_t tlb_read(lnaddr_t addr){
-	tlb_addr tlbaddr;
-	tlbaddr.val = addr;
 	uint32_t i;
 	for(i = 0; i < NR_LINE; ++i){
 		if(tlb[i].valid == 0) break;
-		if(tlb[i].tag == tlbaddr.tag)
-			return (tlb[i].pte.page_frame << 12) + tlbaddr.offset;
+		if(tlb[i].tag == TLB_TAG(addr))
+			return (tlb[i].pte.page_frame << 12) + TLB_OFFSET(addr);
 	}
 	if(i == NR_LINE){
 		srand(time(0));
 		i = rand() % NR_LINE;
 	}
 	tlb[i].valid = true;
-	tlb[i].tag = tlbaddr.tag;
+	tlb[i].tag = TLB_TAG(addr);
 	tlb[i].pte = page_read(addr);
-	return (tlb[i].pte.page_frame << 12) + tlbaddr.offset;
+	return (tlb[i].pte.page_frame << 12) + TLB_OFFSET(addr);
 }
