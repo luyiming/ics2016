2c793166539db8d491f72db8b0fc3cfb4774cb41
tracer-ics2016 <tracer@njuics.org>
2017-01-10 13:34:20 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-57-generic #78-Ubuntu SMP Fri Dec 9 23:50:32 UTC 2016 x86_64 GNU/Linux  13:34:20 up 22:40,  1 user,  load average: 0.74, 0.58, 0.57 9f5999f863c935e89f2c67088245863a6c49ef

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -2,6 +2,7 @@
 
 void init_tlb();
 hwaddr_t tlb_read(lnaddr_t addr);
+uint32_t hwaddr_read(hwaddr_t, size_t);
 
 typedef union {
 	struct {
@@ -12,19 +13,18 @@ typedef union {
 	uint32_t val;
 } addr_page_t;
 
-uint32_t hwaddr_read(hwaddr_t, size_t);
+#define dir_entry(addr) ((addr >> 22) & ((1 << 10) - 1))
+#define page_entry(addr) ((addr >> 12) & ((1 << 10) - 1))
+#define in_page_addr(addr) (addr & ((1 << 12) - 1))
 
 PTE page_read(lnaddr_t addr){
-	addr_page_t lnaddr;
-	lnaddr.val = addr;
-
 	// page directory entry
 	PDE dir_entry;
-	dir_entry.val = hwaddr_read((cpu.CR3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
+	dir_entry.val = hwaddr_read((cpu.CR3.page_directory_base << 12) + 4 * dir_entry(addr), 4);
 
 	// page table entry
 	PTE page_table_entry;
-	page_table_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
+	page_table_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * page_entry(addr), 4);
 
 	return page_table_entry;
 }
@@ -39,17 +39,15 @@ hwaddr_t page_translate(lnaddr_t addr) {
 }
 
 void debug_page(lnaddr_t addr){
-	addr_page_t lnaddr;
-	lnaddr.val = addr;
-	printf("lnaddr = 0x%x, dir = 0x%x, page = 0x%x, offset = 0x%x\n", lnaddr.val, lnaddr.dir, lnaddr.page, lnaddr.offset);
-	printf("CR3.page_directory_base = 0x%x, dir_entry_addr = 0x%x\n", cpu.CR3.page_directory_base, (cpu.CR3.page_directory_base << 12) + 4 * lnaddr.dir);
+	printf("lnaddr = 0x%x, dir = 0x%x, page = 0x%x, offset = 0x%x\n", addr, dir_entry(addr), page_entry(addr), in_page_addr(addr));
+	printf("CR3.page_directory_base = 0x%x, dir_entry_addr = 0x%x\n", cpu.CR3.page_directory_base, (cpu.CR3.page_directory_base << 12) + 4 * dir_entry(addr));
 	PDE dir_entry;
-	dir_entry.val = hwaddr_read((cpu.CR3.page_directory_base << 12) + lnaddr.dir * 4, 4);
+	dir_entry.val = hwaddr_read((cpu.CR3.page_directory_base << 12) + dir_entry(addr) * 4, 4);
 	printf("page directory: present = %x, page_frame << 12 = 0x%x\n", dir_entry.present, dir_entry.page_frame<<12);
 
 	PTE page_table_entry;
-	page_table_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
+	page_table_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * page_entry(addr), 4);
 	printf("page table item: present = %x, page_frame << 12 = 0x%x\n", page_table_entry.present, page_table_entry.page_frame<<12);
 
-	printf("--->hwaddr = 0x%x\n",  (page_table_entry.page_frame << 12) + lnaddr.offset);
+	printf("--->hwaddr = 0x%x\n",  (page_table_entry.page_frame << 12) + in_page_addr(addr));
 }
