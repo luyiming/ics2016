056f9326937f215f9fcec17ffe815a525365ae5f
tracer-ics2016 <tracer@njuics.org>
2017-01-10 13:52:35 +0800

    >  compile NEMU 151220066 luyiming Linux 79fb2e46c627 4.4.0-57-generic #78-Ubuntu SMP Fri Dec 9 23:50:32 UTC 2016 x86_64 GNU/Linux  13:52:35 up 22:58,  1 user,  load average: 0.58, 0.79, 0.83 5f4cdd6ce3a032f1a6f016ca6b1efef9631a5829

--- nemu/src/memory/seg.c
+++ nemu/src/memory/seg.c
@@ -1,13 +1,36 @@
 #include "nemu.h"
 
+#define NR_SREG 4
+
+void add_sreg(uint32_t sreg);
+
 void init_seg() {
 	int i;
-	for(i = 0; i < 4; ++i) {
+	for(i = 0; i < NR_SREG; ++i) {
 		cpu.seg_cache[i].valid = 0;
 	}
 	cpu.seg_cache[R_CS].Base  = 0;
 	cpu.seg_cache[R_CS].Limit = 0xffffffff;
 }
+
+lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg) {
+    // sreg:
+    // 00 ES
+    // 01 CS
+    // 10 SS
+    // 11 DS
+    Assert(sreg >= 0 && sreg < NR_SREG, "sreg index out of range");
+    if(cpu.CR0.protect_enable == 0) {
+		return addr;
+	}
+
+    if(cpu.seg_cache[sreg].valid == 0) {
+		add_sreg(sreg);
+	}
+
+	return cpu.seg_cache[sreg].Base + addr;
+}
+
 
 void add_sreg(uint32_t sreg) {
 	// load seg_descriptor into cache
@@ -17,29 +40,13 @@ void add_sreg(uint32_t sreg) {
     for(i = 0; i < 8; ++i) {
         tmp[i] = lnaddr_read(cpu.GDTR.Base + (cpu.SR[sreg].Index << 3) + i, 1);
     }
+	// Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x", cpu.GDTR.Base + cpu.SR[sreg].Index * 8);
 
     uint32_t Limit = SEG_DESC_LIMIT(segdesc);
 	uint32_t Base = SEG_DESC_BASE(segdesc);
 
-	// Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x", cpu.GDTR.Base + cpu.SR[sreg].Index * 8);
 	cpu.seg_cache[sreg].valid = true;
 	cpu.seg_cache[sreg].Limit = Limit;
 	cpu.seg_cache[sreg].Base  = Base;
 	cpu.seg_cache[sreg].DPL   = segdesc->privilege_level;
-}
-
-lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg) {
-    // sreg:
-    // 00 ES
-    // 01 CS
-    // 10 SS
-    // 11 DS
-    Assert(sreg >= 0 && sreg <= 3, "sreg index out of range");
-    if(cpu.CR0.protect_enable == 0)
-        return addr;
-
-    if(cpu.seg_cache[sreg].valid == 0)
-        add_sreg(sreg);
-
-	return cpu.seg_cache[sreg].Base + addr;
 }
